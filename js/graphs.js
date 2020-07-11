3df44fcfe2e32d032571895e80a46368aa637f78
Forrest Green <greengadgetz@gmail.com>
2020-07-11 09:16:52 -0500

    Make reachable nodes highlighted in animate mode

--- js/graphs.js
+++ js/graphs.js
@@ -157,9 +157,12 @@ The full set of edit mode styles for nodes are:
                 'text-valign': 'center',
                 'color': 'white',
                 'background-color': '#888',
-                'shape': 'circle',
                 'width': 20,
-                'height': 20
+                'height': 20,
+            })
+            .selector('node[?isReachable]')
+            .css({
+                'background-color': '#ff0000',
             })
             .selector('node:selected') //selected node in edit or animate mode
             .css({
@@ -828,8 +831,26 @@ The full set of edit mode styles for nodes are:
                 }
 
                 cy.elements().data("isReachable", false);
-                var reachable = getReachableNodesFrom(cy.elements(), cy.data('lastVisited'));
-                reachable.data("isReachable", true);
+
+                // Update lastVisited in case it changed
+                lastVisited = cy.data('lastVisited');
+
+                var reachableEdges = getReachableEdgesFrom(cy.elements(), lastVisited);
+                reachableEdges.data("isReachable", true);
+
+                // Look at opposite end of each edge and mark the node as reachable
+                reachableEdges.forEach(function(ele, i, eles) {
+                    if(ele.source() === lastVisited) {
+                        ele.target().data("isReachable", true);
+                    }
+                    else if(ele.target() === lastVisited) {
+                        ele.source().data("isReachable", true);
+                    }
+                    else {
+                        // This shouldn't happen!
+                        console.warn("Found reachable edge that doesn't connect to lastVisited!");
+                    }
+                });
             }
         }});
 
@@ -837,7 +858,7 @@ The full set of edit mode styles for nodes are:
         return node1.edgesWith(node2);
     }
 
-    var getReachableNodesFrom = function(graph, node) {
+    var getReachableEdgesFrom = function(graph, node) {
         var available = graph.filter('[!isVisited]');
         if(node == null) {
             return available;
@@ -857,9 +878,11 @@ The full set of edit mode styles for nodes are:
                 candidates = adjacent;
             }
             const nextStitchIsBackStitch = cy.data("nextStitchIsBackStitch");
-            return candidates.filter(function(ele, i, eles) {
+            candidates = candidates.filter(function(ele, i, eles) {
                         return ele.data("isBackStitch") == nextStitchIsBackStitch;
             });
+
+            return candidates;
         }
     };
 
